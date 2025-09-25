a4d047265c6da3b55c0c5c0fdca5aca0c2b36c7f
zion <zion187@naver.com>
2025-09-25 14:48:19 +0900

    💄Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import TickerCard from './TickerCard.jsx'
 
 const stocks = [
-  { ticker: 'NVDA', name: '엔비디아', currentPrice: 178.43, previousClose: 181.40 },
-  { ticker: 'TSLA', name: '테슬라', currentPrice: 425.85, previousClose: 439.95 },
-  { ticker: 'GOOGL', name: '알파벳', currentPrice: 251.66, previousClose: 253.22 },
+  { ticker: 'NVDA', name: '엔비디아', currentPrice: 12308249.43, previousClose: 181.40 },
+  { ticker: 'TSLA', name: '테슬라', currentPrice: 0.85, previousClose: 439.95 },
+  { ticker: 'GOOGL', name: '알파벳', currentPrice: 912049230.66, previousClose: 253.22 },
 //  { ticker: '005930.KS', name: '삼성전자', currentPrice: 85400, previousClose: 84700 },
 ]
 
 function App() {
   return (
-    <div>
-      <h1>오늘의 주식 시세</h1>
-      <div>
+    <div className='bg-purple-900 min-h-screen p-8 flex flex-col items-center'>
+      <h1 className="text-4xl font-thin text-blue-300 mb-10">오늘의 주식 시세</h1>
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full max-w-7xl">
         {
           stocks.map(stock => (
             <TickerCard 
